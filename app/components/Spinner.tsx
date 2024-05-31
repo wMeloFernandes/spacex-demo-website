d5032456f6b21872b9648686a9739403f7387457
Willian Melo <w.melo.fernandes.un@gmail.com>
2024-05-31 12:44:53 -0300

    change text color

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -1,7 +1,7 @@
 export const Spinner = ({ isLoading }: { isLoading: boolean }) => {
     if (isLoading)
         return <div className="flex items-center space-x-2">
-            <span className="text-2xl font-medium text-gray-500">Loading...</span>
+            <span className="text-2xl font-medium text-white">Loading...</span>
         </div>
     return null
 }
