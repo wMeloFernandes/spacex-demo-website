38e43f0a3a430a87471e464b3b629fccc336dc78
Willian Melo <w.melo.fernandes.un@gmail.com>
2024-05-31 04:27:00 -0300

    use new component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import apiCalls from "@/graphql";
 import { ShipsQuery } from "@/util/types/graphql";
 import { useQuery } from "@apollo/client";
+import { ShipCard } from "./components/ShipCard";
 
 export default function Home() {
   const { data } = useQuery<ShipsQuery>(apiCalls.queries.ships, {
@@ -20,13 +21,9 @@ export default function Home() {
   return (
     <main className="flex justify-center min-h-screen p-14">
       <div className="w-full max-w-7xl justify-between flex flex-col gap-2">
-        {data?.ships?.map((ship, index) => {
-          return (
-            <div key={index} className="flex px-4">
-              {ship?.name}
-            </div>
-          );
-        })}
+        {data?.ships?.map((ship) => (
+          <ShipCard key={ship.id} name={ship.name} missions={[]} image={ship.image}></ShipCard>
+        ))}
       </div>
     </main>
   );
