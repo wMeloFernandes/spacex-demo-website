b0eaa4e72200b161428de499a96091fccadb9804
Willian Melo <w.melo.fernandes.un@gmail.com>
2024-05-31 12:32:24 -0300

    fix plural

--- app/components/ShipCard.tsx
+++ app/components/ShipCard.tsx
@@ -10,7 +10,7 @@ export const ShipCard = ({ name, image, missions }: { name: string; image: strin
         <div className="flex flex-col gap-2 py-2">
             <p className="text-xl font-bold">{name}</p>
             <p className="text-gray-500">
-                {`${missions.length} missions`}
+                {`${missions.length} mission${missions.length > 1 ? 's' : ''}`}
             </p>
             <span className="flex items-center justify-start text-gray-500 cursor-pointer hover:underline">
                 <LinkIcon className="h-4 w-4 mr-1" />
