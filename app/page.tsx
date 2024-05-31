a30ef12eac6b7f9606f0d1a60ecaa83b4871cc15
Willian Melo <w.melo.fernandes.un@gmail.com>
2024-05-31 11:00:38 -0300

    add spinner in main component

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,10 @@ import apiCalls from "@/graphql";
 import { ShipsQuery } from "@/util/types/graphql";
 import { useQuery } from "@apollo/client";
 import { ShipCard } from "./components/ShipCard";
+import { Spinner } from "./components/Spinner";
 
 export default function Home() {
-  const { data } = useQuery<ShipsQuery>(apiCalls.queries.ships, {
+  const { data, loading } = useQuery<ShipsQuery>(apiCalls.queries.ships, {
     fetchPolicy: "cache-and-network",
     variables: {
       input: {
@@ -20,11 +21,12 @@ export default function Home() {
 
   return (
     <main className="flex justify-center min-h-screen p-14">
-      <div className="w-full max-w-7xl justify-between flex flex-col gap-2">
+      <Spinner isLoading={loading} />
+      {!loading && <div className="w-full max-w-7xl justify-between flex flex-col gap-2">
         {data?.ships?.map((ship) => (
           <ShipCard key={ship.id} name={ship.name} missions={[]} image={ship.image}></ShipCard>
         ))}
-      </div>
+      </div>}
     </main>
   );
 }
