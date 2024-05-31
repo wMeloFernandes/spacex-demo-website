3c4707a25451fc1933ef328dcf99937c45e1e123
Willian Melo <w.melo.fernandes.un@gmail.com>
2024-05-31 13:05:55 -0300

    centralize content

--- app/components/ShipDetails.tsx
+++ app/components/ShipDetails.tsx
@@ -6,12 +6,9 @@ export const ShipDetails = ({ enabled, onClose }: { enabled: boolean; onClose: (
             <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                 <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-md transition-all sm:my-8 sm:w-full sm:max-w-lg">
                     <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                        <div className="sm:flex sm:items-start">
+                        <div className="flex justify-center">
                             <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                                <h3 className="text-base font-semibold leading-6 text-gray-900">Title</h3>
-                                <div className="mt-2">
-                                    <p className="text-sm text-gray-500">Description</p>
-                                </div>
+                                <h3 className="text-base font-semibold leading-6 text-gray-900">Missions</h3>
                             </div>
                         </div>
                     </div>
