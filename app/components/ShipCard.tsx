af31361e83421cd3ff95ce4b0bcdb94b366faa16
Willian Melo <w.melo.fernandes.un@gmail.com>
2024-05-31 11:11:06 -0300

    add image block

--- app/components/ShipCard.tsx
+++ app/components/ShipCard.tsx
@@ -1,5 +1,7 @@
 export const ShipCard = ({ name, image, missions }: { name: string; image: string | null; missions: any[] }) => {
-    return <div>
-        <span>{name}</span>
+    return <div className="flex gap-3 bg-white border border-gray-300 rounded-xl overflow-hidden items-center justify-start">
+        <div className="relative w-32 h-32 flex-shrink-0">
+            <img className="absolute left-0 top-0 w-full h-full object-cover object-center" src={image ?? ''} />
+        </div>
     </div>
 }
