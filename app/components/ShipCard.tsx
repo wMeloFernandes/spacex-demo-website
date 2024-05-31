03b47101acf7a8b8d0c3fb2bdf26af13fb7e5512
Willian Melo <w.melo.fernandes.un@gmail.com>
2024-05-31 11:13:50 -0300

    add second block for details

--- app/components/ShipCard.tsx
+++ app/components/ShipCard.tsx
@@ -3,5 +3,14 @@ export const ShipCard = ({ name, image, missions }: { name: string; image: strin
         <div className="relative w-32 h-32 flex-shrink-0">
             <img className="absolute left-0 top-0 w-full h-full object-cover object-center" src={image ?? ''} />
         </div>
+        <div className="flex flex-col gap-2 py-2">
+            <p className="text-xl font-bold">{name}</p>
+            <p className="text-gray-500">
+                Description
+            </p>
+            <span className="text-gray-500 cursor-pointer hover:underline">
+                See Details
+            </span>
+        </div>
     </div>
 }
